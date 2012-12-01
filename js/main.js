5f567992ac370798b7b9efede5068a25a1175a44
Martin Atkins <mart@degeneration.co.uk>
2012-12-01 14:23:02 -0800

    Build the main workspace UI using d3

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ function Frame(set) {
     for (var y = 0; y < set.numRows; y++) {
         var row = [];
         for (var x = 0; x < set.numCols; x++) {
-            row.push(this.set.pixelFormat.getDefaultPixelData);
+            row.push(this.set.pixelFormat.getDefaultPixelData());
         }
         this.data.push(row);
     }
@@ -31,6 +31,9 @@ IndexedPixelFormat.prototype = {};
 IndexedPixelFormat.prototype.getDefaultPixelData = function () {
     return 0;
 };
+IndexedPixelFormat.prototype.storageToDisplay = function (idx) {
+    return this.palette[idx];
+};
 
 function RGBPixelFormat(bitsPerChannel) {
     this.bitsPerChannel = bitsPerChannel;
@@ -40,36 +43,54 @@ RGBPixelFormat.prototype.getDefaultPixelData = function () {
     return [0, 0, 0];
 };
 
+function FrameSetView(containerElem) {
+    this.containerElem = containerElem;
+}
+FrameSetView.prototype = {};
+FrameSetView.prototype.update = function (frameSet) {
+    var update = d3.select(this.containerElem).selectAll(".frame").data(frameSet.frames);
+
+    var enter = update.enter().append("div");
+    enter.attr("class", "frame");
+
+    var rowsUpdate = update.selectAll(".matrix-row").data(function (d) {
+        // The set of rows from each frame.
+        return d.data;
+    });
+    var rowsEnter = rowsUpdate.enter().append("div");
+    rowsEnter.attr("class", "matrix-row");
+    rowsUpdate.exit().remove();
+
+    var colsUpdate = rowsUpdate.selectAll(".matrix-cell").data(function (d) {
+        // The pixel data from each row in storage (not display) format.
+        return d;
+    });
+    var colsEnter = colsUpdate.enter().append("div");
+    colsEnter.attr("class", "matrix-cell");
+    colsUpdate.style("background-color", function (d) {
+        // Input is a pixel in storage format. We must use the frameset's
+        // pixel format to convert to an RGB value we can actually render,
+        // and then finally convert that into a CSS color value.
+        var dispColor = frameSet.pixelFormat.storageToDisplay(d);
+        return "rgb(" + dispColor.join(",") + ")";
+    });
+    colsUpdate.exit().remove();
+
+    update.exit().remove();
+};
+
 var frameSet = null;
+var frameSetView = null;
 
 function init() {
 
     var pixelFormat = new IndexedPixelFormat([[0, 0, 0], [255, 0, 0]]);
-    var frameSet = new FrameSet(8, 24, pixelFormat, 1);
+    var frameSet = new FrameSet(8, 5, pixelFormat, 4);
 
-    for (var r = 0; r < frameSet.numRows; r++) {
-        var $row = $("<div class='matrix-row'></div>");
-        for (var c = 0; c < frameSet.numCols; c++) {
-            var $cell = $("<div class='matrix-cell'></div>");
-            $row.append($cell);
-        }
-        $("#matrix").append($row);
-    }
+    var frameSetView = new FrameSetView(document.getElementById("workspace"));
 
-    $(".matrix-cell").bind('click', function () {
-        var $this = $(this);
+    frameSetView.update(frameSet);
 
-        console.log("It's ", this.style.backgroundColor);
-
-        if (this.style.backgroundColor != "") {
-            console.log("off");
-            $this.css("background-color", "");
-        }
-        else {
-            console.log("on");
-            $this.css("background-color", "red");
-        }
-    });
 }
 
 
