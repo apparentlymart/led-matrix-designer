d7cc455539a9c98a50fcecb3ca647c670bd8d6ef
Martin Atkins <mart@degeneration.co.uk>
2012-12-01 09:34:34 -0800

    Start to define a real data model for framesets, frames and pixels.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,55 @@
 
-var numRows = 8;
-var numCols = 24;
+function FrameSet(numRows, numCols, pixelFormat, numFrames) {
+    this.numRows = numRows;
+    this.numCols = numCols;
+    this.pixelFormat = pixelFormat;
 
-function initMatrix() {
+    if (numFrames == null) numFrames = 1;
 
-    for (var r = 0; r < numRows; r++) {
+    this.frames = [];
+    for (var i = 0; i < numFrames; i++) {
+        this.frames.push(new Frame(this));
+    }
+}
+
+function Frame(set) {
+    this.set = set;
+    this.data = [];
+    for (var y = 0; y < set.numRows; y++) {
+        var row = [];
+        for (var x = 0; x < set.numCols; x++) {
+            row.push(this.set.pixelFormat.getDefaultPixelData);
+        }
+        this.data.push(row);
+    }
+}
+
+function IndexedPixelFormat(palette) {
+    this.palette = palette;
+}
+IndexedPixelFormat.prototype = {};
+IndexedPixelFormat.prototype.getDefaultPixelData = function () {
+    return 0;
+};
+
+function RGBPixelFormat(bitsPerChannel) {
+    this.bitsPerChannel = bitsPerChannel;
+}
+RGBPixelFormat.prototype = {};
+RGBPixelFormat.prototype.getDefaultPixelData = function () {
+    return [0, 0, 0];
+};
+
+var frameSet = null;
+
+function init() {
+
+    var pixelFormat = new IndexedPixelFormat([[0, 0, 0], [255, 0, 0]]);
+    var frameSet = new FrameSet(8, 24, pixelFormat, 1);
+
+    for (var r = 0; r < frameSet.numRows; r++) {
         var $row = $("<div class='matrix-row'></div>");
-        for (var c = 0; c < numCols; c++) {
+        for (var c = 0; c < frameSet.numCols; c++) {
             var $cell = $("<div class='matrix-cell'></div>");
             $row.append($cell);
         }
@@ -30,4 +73,4 @@ function initMatrix() {
 }
 
 
-$("#matrix").ready(initMatrix);
+$("#matrix").ready(init);
